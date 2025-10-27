5d6aed767212ac3e5a0b2c0d7773d1f7ab1e8e8d
yucheng-wu <41343109@nfu.edu.tw>
2025-10-27 13:49:52 +0800

    Update cbuiltinblg.cpp

--- cbuiltinblg.cpp
+++ cbuiltinblg.cpp
@@ -13,6 +13,19 @@ cbuiltinblg::cbuiltinblg(QWidget *parent)
     pagePushBtn      = new QPushButton(QStringLiteral("頁面設定對話盒"));
     progressPushBtn  = new QPushButton(QStringLiteral("進度對話盒"));
     printPushBtn     = new QPushButton(QStringLiteral("列印對話盒"));
+    gridLayout->addWidget(colorPushBtn,0,0,1,1);
+    gridLayout->addWidget(errorPushBtn,0,1,1,1);
+    gridLayout->addWidget(filePushBtn,0,2,1,1);
+    gridLayout->addWidget(fontPushBtn,1,0,1,1);
+    gridLayout->addWidget(inputPushBtn,1,1,1,1);
+    gridLayout->addWidget(pagePushBtn,1,2,1,1);
+    gridLayout->addWidget(progressPushBtn,2,0,1,1);
+    gridLayout->addWidget(printPushBtn,2,1,1,1);
+    gridLayout->addWidget(displayTextEdit,3,0,3,3);
+
+    setLayout(gridLayout);
+    setWindowTitle(QStringLiteral("內建對話盒展示"));
+    resize(400,300);
 }
 
 cbuiltinblg::~cbuiltinblg() {}
